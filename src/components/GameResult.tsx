5e86424b6472dd39507f1879c12ad2546078bbe7
Hashim Rafiq <hashim.rafiq332@gmail.com>
2025-04-07 17:37:13 +0100

    making stock price component

--- src/components/GameResult.tsx
+++ src/components/GameResult.tsx
@@ -1,6 +1,7 @@
 type GameResultProps = {
   gameResult: string;
 };
+
 export default function GameResult({ gameResult }: GameResultProps) {
   return (
     <h3 className="text-2xl font-semibold animate-glows mt-6 pb-8">
